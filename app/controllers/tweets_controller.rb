c8f196d7efe0c7aea75c70b52bd004119525c239
Zuma-coder <munieru3810@gmail.com>
2024-06-09 16:50:38 +0900

    静的なページを作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,8 @@ class TweetsController < ApplicationController
   def index
     @user = current_user
   end
+
+  def following
+    @user = current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
 
   root to: 'tweets#index'
 
-  resources :tweets
+  resources :tweets do
+    get :following, on: :collection
+  end
 
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
